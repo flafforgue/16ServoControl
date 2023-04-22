5184d777f48a539e35d1e960de8ef22039b2af0a
flafforgue <30392727+flafforgue@users.noreply.github.com>
2023-04-23 00:44:38 +0200

    First Version

--- 16ServoControl.ino
+++ 16ServoControl.ino
@@ -1,11 +1,33 @@
 // -------------------------------------------------------------
 //
+// 16 Servos Controler
+// Version 1.00  first complete version
+// (c) 2023 F.LAFFORGUE
 //
-// 19/04/2023  Flash :  21844  Mem : 857   Reste : 1191 // 2 lines - ok
-// 20/04/2023  Flash :  21714  Mem : 729   Reste : 1319 // 4 lines - Change sequence from int to byte
-// 20/04/2023  Flash :  21681  Mem : 681   Reste : 1367 // all changed from int to byte
-//                                   713           1335 // 6 lines - ok
+// Need about 1200 byte free space to run
+//
 // 21/04/2023  Flash :  21862  Mem : 713   Reste : 1335
+// 22/04/2023  Flash :  23220  Mem : 785   Reste : 1263
+//
+// -------------------------------------------------------------
+
+// Configuration options
+
+                           // Range can be reduced in Setup
+#define MIN_US   50        //   500 us  
+#define MAX_US  250        //  2500 us
+
+#define SERIALDEBUG        // use 444o flash and 20o Ram more
+
+#define CONFIGMOVESERVO    // Moving Servos when config
+                           // Be carful as May damage Servo
+                           // pulse may vary from 500 to 2500 us
+                           
+#define CONFIGAUTOCHANGE   // Automaticly change Chanel when Pot moved
+                           // Otherwise change by rotating encoder
+
+#define MODELOOP           // Loop in play mode until "Btn Press"
+
 // -------------------------------------------------------------
 
 #define OSCFREQUENCY 28729000
@@ -56,7 +78,7 @@ Adafruit_SSD1306 OLed(OLedWidth, OLedHight, &Wire, OLedReset);
 
 byte Values[16];
 byte LastChanelchanged = 0;
-
+  
 const byte ch0[16] PROGMEM = { 0,0,0,0, 1,1,1,1, 0,0,0,0, 1,1,1,1 };
 const byte ch1[16] PROGMEM = { 0,0,0,0, 0,0,0,0, 1,1,1,1, 1,1,1,1 };
 const byte ch2[16] PROGMEM = { 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1 };
@@ -85,9 +107,6 @@ void ReadChannels(){                // Read all Channels
 // -------------------------------------------------------------
 //  Set Servo values
 // -------------------------------------------------------------
-
-#define MIN_US   50    //   500 us
-#define MAX_US  250    //  2500 us
 
 byte MinUs[16];  //  
 byte MaxUs[16];  //  
@@ -198,63 +217,9 @@ byte readkey() {
   return( tmp);
 }
 
-// -------------------------------------------------------------
-// Sauvegarde et restauration eeprom
-// -------------------------------------------------------------
-
-const int ProgStamp = 3219;
-
-#define Adr_Stamp  0
-#define Adr_MinUs  2
-#define Adr_MaxUs 18
-
-void InitVars() {
-  for ( byte i=0; i<16 ; i++ ) {
-    MinUs[i]=MIN_US;
-    MaxUs[i]=MAX_US;   
-  }
-}
-
-void SaveToEprom() {
-  EEPROM.put(Adr_Stamp, ProgStamp);
-  EEPROM.put(Adr_MinUs, MinUs);
-  EEPROM.put(Adr_MaxUs, MaxUs); 
-  Serial.println(F("Save eeprom"));
-}
-
-void InitFromEprom () {
-  int temp;
-  EEPROM.get(Adr_Stamp, temp);
-  if ( temp == ProgStamp ) {  // Stamp found , we can assume eerom contain Valid Datas 
-    Serial.println(F("Stamp Found"));
-    Serial.println(F("Read eeprom"));
-    EEPROM.get(Adr_MinUs, MinUs);
-    EEPROM.get(Adr_MaxUs, MaxUs);
-
-    Serial.print("UsMin : ");
-    for (byte i=0;i<16;i++ ) {
-      Serial.print(MinUs[i]);    
-      Serial.print(",");  
-    }
-    Serial.println("");
-
-    Serial.print("UsMax : ");
-    for (byte i=0;i<16;i++ ) {
-      Serial.print(MaxUs[i]);    
-      Serial.print(",");  
-    }
-    Serial.println("");
-  
-  } else {                    // Stamp not found , Values need to be initialized
-    Serial.println(F("Stamp Not Found"));
-    InitVars();
-    SaveToEprom(); 
-  }
-}
-
-// -------------------------------------------------------------
+// =============================================================
 //                  M E N U    &   D I S P L A Y
-// -------------------------------------------------------------
+// =============================================================
 
 #define L1       0
 #define L2      16
@@ -262,8 +227,7 @@ void InitFromEprom () {
 #define L4      48
 #define Lmargin  4
 
-void TextMenu(String str) {
-//  Serial.println(str);
+void TitleMenu(String str) {
   OLed.clearDisplay();
   OLed.setTextSize(2);
   OLed.setTextColor(SSD1306_WHITE);
@@ -271,11 +235,17 @@ void TextMenu(String str) {
   OLed.println(str); 
 }
 
+void Saved() {
+  TitleMenu(F("Saved"));
+  OLed.display();
+  delay(750);
+}
+
 void OLedprint2 ( int value ) {
-//  char buff[3];
+//  char buff[3];                 
 //  sprintf(buff, "%2d", value);
 //  OLed.print(buff);
-   if (( value ) < 10 ) { OLed.print(F(" ")); } 
+   if (( value ) < 10 ) { OLed.print(F(" ")); }    // finaly not using sprintf , use more size
    OLed.print(value);
 }
 
@@ -283,15 +253,30 @@ void OLedprint4 ( int value ) {
 //  char buff[5];
 //  sprintf(buff, "%4d", value);
 //  OLed.print(buff);
-   if (( value ) < 1000 ) { OLed.print(F(" ")); } 
+   if (( value ) < 1000 ) { OLed.print(F(" ")); }  // finaly not using sprintf , use more size
    if (( value ) <  100 ) { OLed.print(F(" ")); }   
    if (( value ) <   10 ) { OLed.print(F(" ")); }
    OLed.print(value);
 }
 
-// -------------------------------------------------------------
-//  Mode Live
-// -------------------------------------------------------------
+void WaitToStart() {
+  boolean WaitToStart = true;   
+
+  OLed.setCursor(4, L1);  OLed.print(F("Press Sel"));
+  OLed.setCursor(8, L2);  OLed.print(F("to start"));
+  OLed.display();
+  
+  while ( WaitToStart ) {
+    ReadBtnState();
+    if ( readkey() == BTN_SELECT ) { 
+      WaitToStart=false;
+    }
+  }
+}
+
+// =============================================================
+//                          Mode Live
+// =============================================================
 
 void DoLive() {
   boolean       LetRunning = true;
@@ -306,7 +291,7 @@ void DoLive() {
     }
 
     if ( millis() - omillis > 150 ) { // Refresh Display every 150 us
-      TextMenu(F("Live"));
+      TitleMenu(F("Live"));
       int microsec = map(Values[LastChanelchanged], 0, 255, MinUs[LastChanelchanged],  MaxUs[LastChanelchanged]);
       OLed.setCursor(4 ,L4);  OLedprint2(LastChanelchanged);
       OLed.setCursor(68,L4);  OLedprint4(microsec*10);
@@ -318,43 +303,37 @@ void DoLive() {
   ClearEncoder();
 }
 
-// -------------------------------------------------------------
+// =============================================================
 //          P L A Y  &  R E C C O R D    S E Q U E N C E 
-// -------------------------------------------------------------
+// =============================================================
 
-#define NbStep  10
-#define NbLines  6
-byte Sequence [NbLines][16] = {
-  { 000,255,000,127,127,127,127,000,000,000,000,000,000,000,000,000 },
-  { 255,000,255,127,127,127,000,000,000,000,000,000,000,000,000,000 },
-  { 000,255,000,127,127,000,000,000,000,000,000,000,000,000,000,000 },
-  { 255,000,255,127,000,000,000,000,000,000,000,000,000,000,000,000 },
+
+#define NbStep    10      // NB steps for fadding ( change Speed ) 
+#define MaxLines  10
+byte Sequence [MaxLines][16] = {
   { 000,255,000,000,000,000,000,000,000,000,000,000,000,000,000,000 },
-  { 255,000,255,000,000,000,000,000,000,000,000,000,000,000,000,000 } };
+  { 255,000,255,000,000,000,000,000,000,000,000,000,000,000,000,000 },
+  { 000,255,000,000,000,000,000,000,000,000,000,000,000,000,000,000 },
+  { 255,000,255,000,000,000,000,000,000,000,000,000,000,000,000,000 },
+  { 000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000 },
+  { 000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000 },
+  { 000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000 },
+  { 000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000 },
+  { 000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000 },
+  { 000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000 } };
+byte   NbLines   = MaxLines;
 
-// int Current[16]; // deleted use Values
-int  Speed = 500;
-//byte Seq[16];
-
+int           Speed = 500;  // millisecond between steps
 unsigned long Lck;
-
-void InitCurentSeq(byte line) {
-//  for ( byte i=0; i<16; i++ ) {
-//    Seq[i] = Sequence[line][i];
-//  }
-  Lck = 0xFFFF;
-}
 
 void UnlockChanels(byte line) {
   unsigned long temp;
   for (byte i=0; i<16; i++ ) {
-//    if ( abs ( (int) Seq[i] - (int) Values[i] ) < 2 ) {
     if ( abs ( (int) Sequence[line][i] - (int) Values[i] ) < 2 ) {
       temp = ~( 1 << i );
       Lck  =  Lck & temp ;     
     }
     if (( Lck & ( 1 << i) ) == 0 ) {
-//      Seq[i] = Values[i];
       Sequence[line][i] = Values[i];
     }
   } 
@@ -379,7 +358,7 @@ void DoRecord() {
 
   while ( LetRunning ) {
     if ( oLine != CurLine ) {
-      InitCurentSeq(CurLine);
+      Lck   = 0xFFFF;
       oLine = CurLine;
     }
     ReadChannels();
@@ -392,19 +371,21 @@ void DoRecord() {
                             break;
       case BTN_LEFT:        CurLine--;
                             break;    
-      case BTN_SELECT:      
-      case BTN_SELECT_Long: LetRunning=false;                        
+      case BTN_SELECT_Long: NbLines=CurLine+1;
+                            SaveSequence();
+                            Saved();                              
+      case BTN_SELECT:      LetRunning=false;
     }
-    if ( CurLine <       0 ) { CurLine =       0; }
-    if ( CurLine > CurLine ) { CurLine = CurLine; }
+    if ( CurLine <        0 ) { CurLine =        0; }
+    if ( CurLine > MaxLines ) { CurLine = MaxLines; }
     
     if ( millis() - omillis > 150 ) {  // Refresh Display every 150 us
-      TextMenu(F("Reccord"));
+      TitleMenu(F("Reccord"));
       int microsec = map(Values[LastChanelchanged], 0, 255, MinUs[LastChanelchanged],  MaxUs[LastChanelchanged]);
     
       for ( byte j=0; j<16 ; j++ ) {          // Mark Locked Channels
-         if ( ( Lck & ( 1 << j) ) != 0 ) {
-           OLed.fillRect(8 + j*6, L2+3, 5, 8, SSD1306_WHITE);      
+        if ( ( Lck & ( 1 << j) ) != 0 ) {
+          OLed.fillRect(8 + j*6, L2+3, 5, 8, SSD1306_WHITE);      
         }
       }
       OLed.setTextColor(SSD1306_INVERSE);
@@ -424,37 +405,35 @@ void DoRecord() {
   }
 
   ClearEncoder();
-//  Serial.println(F("Leave Live"));
 }
 
 // -------------------------------------------------------------
 
 void setCurrent( int Line, int Step ) {
   int tmp;
-//  Serial.print(Line);   Serial.print(" , ");
-//  Serial.print(Step);   Serial.print(" : ");
-//  Serial.print(Sequence[Line][0]);   Serial.print(" : ");
   for (byte i=0; i<16; i++ ) {
     tmp = (( int ) ( NbStep-1 -  Step) * (int) Sequence[Line][i]  +  (int )( Step ) * ( int ) Sequence[(Line+1) % NbLines][i])/10;
     Values[i]= tmp; 
-//    Serial.print(tmp);   Serial.print(" , ");
-//    Values[1]= (( Step * Sequence[Line][i] + ( NbStep-1 - Step ) * Sequence[Line][i] ) *10 )/ NbStep;
   }
-//  Serial.println("");
 }
+
+// -------------------------------------------------------------
 
 void DoPlay() { 
   boolean WaitToStart = true;
   boolean LetRunning  = true;
+  
   while ( WaitToStart ) {
-    ReadBtnState();
-    OLed.clearDisplay();
-    OLed.setTextSize(2);
-    OLed.setCursor(4, L1);  OLed.print(F("Press Sel"));
-    OLed.setCursor(8, L2);  OLed.print(F("to start"));
+    TitleMenu(F("Play"));   
+//    OLed.clearDisplay();
+//    OLed.setTextSize(2);
+    OLed.setCursor(4, L2);  OLed.print(F("Press Sel"));
+    OLed.setCursor(8, L3);  OLed.print(F("to start"));
     OLed.setCursor(4, L4);  OLed.print(F("speed"));
     OLed.setCursor(68,L4);  OLedprint4(Speed);
     OLed.display();
+
+    ReadBtnState();
     if ( readkey() == BTN_SELECT ) { 
       WaitToStart=false;
     }
@@ -472,7 +451,7 @@ void DoPlay() {
   byte          cline = 0;
   byte          cstep = 0;
   unsigned long otime = millis();
-  
+    
   while ( LetRunning ) {
     ReadBtnState();
     if ( readkey() == BTN_SELECT ) { // if Select Pressed exit
@@ -480,18 +459,24 @@ void DoPlay() {
     }
     setCurrent( cline, cstep );
     UpdateServos();
-//    for(byte i=0;i<16;i++) {
-//      SetServo(i, Values[i] );
-//    }      
-    if ( disp) {
-      OLed.clearDisplay();
-      OLed.setTextSize(2);
-      OLed.setCursor( 4, L1);  OLed.print(F("Running"));
-      OLed.setCursor(20, L2);  OLedprint2(cline);
-      OLed.setCursor(68, L2);  OLedprint2(cstep);
+
+    if (encodermov != 0 ) {
+      cli();
+      Speed+=encodermov*10;
+      encodermov=0;
+      sei();
+      if ( Speed > 1000 ) { Speed = 1000; }
+      if ( Speed <    0 ) { Speed =    0; }
+    }   
+          
+    if ( disp ) {
+      TitleMenu(F("Running"));
+      OLed.setCursor(20, L2);  OLedprint2(cline+1);
+      OLed.setCursor(68, L2);  OLedprint2(cstep+1);
       OLed.setCursor( 4, L4);  OLed.print(F("speed"));
       OLed.setCursor(68, L4);  OLedprint4(Speed);
       OLed.display();
+      disp=false;
     }
     unsigned long t0=millis();
     if ( ( t0 - otime ) >= Speed ) {
@@ -503,7 +488,11 @@ void DoPlay() {
         cline++;
         if ( cline >= NbLines ) {
           cline=0;
+          
+#ifndef MODELOOP          
           LetRunning=false;
+#endif
+
         }
       }
     }  
@@ -516,19 +505,29 @@ void DoPlay() {
   ClearEncoder(); 
 }
 
-// -------------------------------------------------------------
-//                  C O N F I G U R A T I O N
-// -------------------------------------------------------------
+// =============================================================
+//            P R O G R A M    C O N F I G U R A T I O N
+// =============================================================
 
-void DoSetup() {
+void DoConfig() {
   boolean   LetRunning  = true;
   char      Channel     = 0;
   byte      AnaVal;
   int       us;
-  
+
+#ifdef CONFIGMOVESERVO
+   // Todo : add message to set all pots to middle
+#endif
+
   while ( LetRunning ) {
-    ReadBtnState();
-    
+    TitleMenu(F("Live"));
+    OLed.setCursor(4, L3);  OLed.print(F("C ")); OLedprint2( (byte) Channel);
+    OLed.setCursor(4, L4);  OLedprint4(us*10);      
+    OLed.setCursor(68,L3);  OLedprint4(MinUs[Channel]*10);
+    OLed.setCursor(68,L4);  OLedprint4(MaxUs[Channel]*10);
+    OLed.display();     
+  
+    ReadBtnState();   
     switch ( readkey() ) {
       case BTN_RIGHT:       MaxUs[Channel]=us;
               break;     
@@ -541,10 +540,12 @@ void DoSetup() {
       case BTN_SELECT:      LetRunning=false;
               break;
       case BTN_SELECT_Long: SaveToEprom();
+                            Saved();
                             LetRunning=false;
               break;
     }
-             
+
+#ifndef CONFIGAUTOCHANGE
     if (encodermov != 0 ) {
       cli();
       Channel+=encodermov;
@@ -554,26 +555,123 @@ void DoSetup() {
       if ( Channel > 15 ) { Channel =  0; }
     }
     AnaVal=ReadChannel(Channel);
-    us    =map(AnaVal, 0, 255, MIN_US,MAX_US);
+#else
+    ReadChannels();
+    AnaVal =Values[Channel];
+    Channel=LastChanelchanged;
+#endif
+
+    us=map(AnaVal, 0, 255, MIN_US,MAX_US);
     
-    OLed.clearDisplay();
-    OLed.setTextColor(SSD1306_WHITE);
-    OLed.setCursor(4, L1);  OLed.print(F("Setup"));
-    OLed.setCursor(4, L3);  OLed.print(F("C ")); OLedprint2( (byte) Channel);
-    OLed.setCursor(4, L4);  OLedprint4(us*10);      
-    OLed.setCursor(68,L3);  OLedprint4(MinUs[Channel]*10);
-    OLed.setCursor(68,L4);  OLedprint4(MaxUs[Channel]*10);
-    OLed.display(); 
+#ifdef CONFIGMOVESERVO
+    pwm.writeMicroseconds(Channel, us*10); 
+#endif
+    
   }
   ClearEncoder();
 }
-    
-// -------------------------------------------------------------
+
+// =============================================================
+//                    eeprom Recal and Save 
+// =============================================================
+
+const int ProgStamp = 3220;
+
+#define Adr_Stamp  0
+#define Adr_MinUs  2
+#define Adr_MaxUs 18
+#define Adr_Seque 34
+
+void InitVars() {
+  for ( byte i=0; i<16 ; i++ ) {
+    MinUs[i]= 60; // By default set to  600 us min
+    MaxUs[i]=240; // By default set to 2400 us Max
+  }
+}
+
+void SaveToEprom() {
+  EEPROM.put(Adr_Stamp, ProgStamp);
+  EEPROM.put(Adr_MinUs, MinUs);
+  EEPROM.put(Adr_MaxUs, MaxUs); 
+//  Serial.println(F("Save eeprom"));
+}
+
+void SaveSequence() {
+  EEPROM.put(Adr_Seque  , NbLines);
+  EEPROM.put(Adr_Seque+1, Sequence); 
+
+#ifdef SERIALDEBUG
+  Serial.println();
+  Serial.println(NbLines);
+  for (byte i=0; i<MaxLines ; i++) {
+    Serial.print(i+1);
+    Serial.print(" : ");                              
+    for (byte j=0; j<16 ; j++) {                        
+      Serial.print(Sequence[i][j]);
+      Serial.print(" , "); 
+    }
+    Serial.println();
+  }
+#endif
+}
+
+void InitFromEprom () {
+  int temp;
+  EEPROM.get(Adr_Stamp, temp);
+  if ( temp == ProgStamp ) {  // Stamp found , we can assume eerom contain Valid Datas 
+    EEPROM.get(Adr_MinUs  , MinUs);      // Get Minimal Servo pos
+    EEPROM.get(Adr_MaxUs  , MaxUs);      // Get Maximal Servo pos  
+    EEPROM.get(Adr_Seque  , NbLines);    // Get Nb Sequences actives 
+    EEPROM.get(Adr_Seque+1, Sequence);   // Get  Sequences
+  
+#ifdef SERIALDEBUG
+    Serial.print("UsMin : ");
+    for (byte i=0;i<16;i++ ) {
+      Serial.print(MinUs[i]);    
+      Serial.print(",");  
+    }
+    Serial.println("");
+
+    Serial.print("UsMax : ");
+    for (byte i=0;i<16;i++ ) {
+      Serial.print(MaxUs[i]);    
+      Serial.print(",");  
+    }
+    Serial.println("");
+
+    Serial.println();
+    Serial.println(NbLines);
+    for (byte i=0; i<MaxLines ; i++) {
+      Serial.print(i+1);
+      Serial.print(" : ");                              
+      for (byte j=0; j<16 ; j++) {                        
+        Serial.print(Sequence[i][j]);
+        Serial.print(" , "); 
+      }
+      Serial.println();
+    }
+#endif
+  
+  } else {                    // Stamp not found , Values need to be initialized
+#ifdef SERIALDEBUG
+    Serial.println(F("Stamp Not Found"));
+#endif
+
+    InitVars();
+    SaveToEprom(); 
+    SaveSequence();
+  }
+}
+
+// =============================================================
 //                 I N I T I A L I Z A T I O N
-// -------------------------------------------------------------
+// =============================================================
 
 void setup(){
+  
+#ifdef SERIALDEBUG
   Serial.begin(115200);
+#endif
     
   pinMode(ROT_A      , INPUT_PULLUP);
   pinMode(ROT_B      , INPUT_PULLUP);
@@ -598,28 +696,29 @@ void setup(){
   InitFromEprom();  
   
   if(!OLed.begin(SSD1306_SWITCHCAPVCC, OLedAdr)) {
+#ifdef SERIALDEBUG
     Serial.println(F("SSD1306 Error"));
+#endif 
   } else {
+#ifdef SERIALDEBUG
     Serial.println(F("SSD1306 Initialized"));
+#endif
   }
   OLed.display();  // display adafruit logo
 
   attachInterrupt(digitalPinToInterrupt(ROT_A), doEncoder, CHANGE );  
 
-//  for (byte i=0; i<NbLines ; i++ ) {
-//    for ( byte j=0; j<16 ; j++ ) {
-//      Sequence[i][j]=127;
-//    }
-//  }
-
+#ifdef SERIALDEBUG
   Serial.println(F("Running ..."));
+#endif
+
   delay(2000);
   ClearEncoder();
 }
 
-// -------------------------------------------------------------
+// =============================================================
 //                     M A I N    L O O P
-// -------------------------------------------------------------
+// =============================================================
 
 int menu  = 0;
 int omenu = 2;
@@ -658,7 +757,7 @@ void loop(){
                 break;        
         case 2: DoRecord(); 
                 break;
-        case 3: DoSetup();
+        case 3: DoConfig();
                 break;                                       
       }
     }
